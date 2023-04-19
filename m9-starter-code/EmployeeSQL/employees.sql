423c5b25128a12d78f815bb5e1ff95707602d24a
Leigha <lsrussell834@gmail.com>
2023-04-19 13:38:26 -0700

    Final employees SQL file

--- m9-starter-code/EmployeeSQL/employees.sql
+++ m9-starter-code/EmployeeSQL/employees.sql
@@ -87,9 +87,43 @@ e.emp_no=de.emp_no
 INNER JOIN departments as d ON
 de.dept_no=d.dept_no;
 
---List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+--List first name, last name, and sex of each employee whose first name is Hercules and whose 
+--last name begins with the letter B.
 SELECT first_name, last_name, sex
 FROM employees
 WHERE first_name = 'Hercules' 
 AND last_name LIKE 'B%';
 
+-- List each employee in the Sales department, including their employee number, last name, and first name.
+SELECT emp_no, last_name, first_name
+FROM employees
+WHERE emp_no 
+IN(
+	SELECT emp_no
+	FROM dept_emp
+	WHERE dept_no 
+	IN(
+		SELECT dept_no
+		FROM departments
+		WHERE dept_name = 'Sales'));
+		
+--List each employee in the Sales and Development departments, including their employee number, 
+--last name, first name, and department name.
+CREATE VIEW employee_departments AS
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees as e
+INNER JOIN dept_emp as de ON
+e.emp_no=de.emp_no
+INNER JOIN departments as d ON
+de.dept_no=d.dept_no;
+
+SELECT * 
+FROM employee_departments
+WHERE dept_name IN ('Sales', 'Development');
+
+-- List the frequency counts, in descending order, of all the employee last names 
+--(that is, how many employees share each last name).
+SELECT last_name, COUNT(last_name) AS "employee count"
+FROM employees
+GROUP BY last_name
+ORDER BY "employee count" DESC;
